d1c0fd6904e852c862c133c014b9ae3eac076ada
manu <manu.web3.0@gmail.com>
2023-09-25 20:43:23 -0600

    Added more explanation

--- contracts/interfaces/IUniswapV2ERC20.sol
+++ contracts/interfaces/IUniswapV2ERC20.sol
@@ -5,19 +5,37 @@ interface IUniswapV2ERC20 {
     event Transfer(address indexed from, address indexed to, uint value);
 
     function name() external pure returns (string memory);
+
     function symbol() external pure returns (string memory);
+
     function decimals() external pure returns (uint8);
+
     function totalSupply() external view returns (uint);
+
     function balanceOf(address owner) external view returns (uint);
+
     function allowance(address owner, address spender) external view returns (uint);
 
     function approve(address spender, uint value) external returns (bool);
+
     function transfer(address to, uint value) external returns (bool);
+
     function transferFrom(address from, address to, uint value) external returns (bool);
 
+    // Returns a domain separator for use in permit.
     function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    // Returns the current nonce for an address for use in permit.
     function nonces(address owner) external view returns (uint);
 
+    // Sets the allowance for a spender where approval is granted via a signature.
+    // Supporting meta transactions
+    // This obviates the need for a blocking approve transaction before programmatic interactions with pool tokens can occur.
+    // using ERC-712
+    // https://docs.uniswap.org/contracts/v2/guides/smart-contract-integration/supporting-meta-transactions
+    // Permit = No more "Blocking Approve Transaction"
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
 }
--- contracts/interfaces/IUniswapV2Factory.sol
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -1,22 +1,36 @@
 pragma solidity >=0.5.0;
 
 interface IUniswapV2Factory {
+    // Emitted each time a pair is created via createPair()
+    // token0 is guaranteed to be strictly less than token1 by sort order...
+    // despite the fact that tokensA,B were given as input arguments to createPair() out-of-order
+    // The final uint log value will be 1 for the first pair created, 2 for the second, etc. (see allPairs/getPair).
+    // but how come multiple pairs - all are different-tokens pairs
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
+    // send the fee to this address, if feeOn == true
     function feeTo() external view returns (address);
 
+    // The address allowed to change feeTo.
     function feeToSetter() external view returns (address);
 
+    /*  Returns the address of the pair for tokenA and tokenB, if it has been created, else address(0) 
+        tokenA and tokenB are interchangeable.  */
+    // no such mandate of the Pair be created thru the Factory
     function getPair(address tokenA, address tokenB) external view returns (address pair);
 
+    /*  Returns the address of the nth pair (0-indexed) created THRU THE FACTORY, or address(0)... 
+        if not enough pairs have been created yet.
+        Pass 0 for the address of the first pair created, 1 for the second, etc.    */
     function allPairs(uint) external view returns (address pair);
 
+    // Returns the total number of pairs created THRU THE FACTORY so far.
     function allPairsLength() external view returns (uint);
 
     // critical function - deploys UniswapV2Pair.sol for a new pair of assets
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
-    function setFeeTo(address) external;
+    function setFeeTo(address) external; // shouldn't there be a modifier that only feeToSetter can set it
 
-    function setFeeToSetter(address) external;
+    function setFeeToSetter(address) external; // sets the permission to adjust feeTo()
 }
